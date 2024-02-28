bd1346d6caa637cd12ace4677523adb3c7029233
rajeevtensorblue <152684811+rajeevtensorblue@users.noreply.github.com>
2024-02-28 16:53:24 +0530

    Update baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,4 @@
-//const baseUrl = "https://ideaverse-server-v1-yhti.onrender.com";
+const baseUrl = "https://ideaverse-server-v1-yhti.onrender.com";
 
-const baseUrl = "http://localhost:8000";
+// const baseUrl = "http://localhost:8000";
 export default baseUrl;
